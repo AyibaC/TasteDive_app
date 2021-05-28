116563b99fbb2eb380fa48ec594e6de0b9bb58ce
Ayiba Cesario <ayibacesario@Ayibas-MacBook-Air.local>
2021-05-28 21:00:35 +0100

    fixed list not appearing

---
+++ public/scripts/main.js
@@ -0,0 +1,75 @@
+console.log('hooked up');
+const searchForm = document.forms["search-form"];
+
+function renderList(taste){
+    const helloMessage = document.getElementById('hello-message');
+    helloMessage.remove();
+    const resultMessage = document.getElementById('result-message');
+    resultMessage.innerHTML = ''
+    const msg = document.createElement("p");
+    msg.classList.add('list-heading')
+    const userTaste = JSON.parse(localStorage.getItem('userTaste'));
+    msg.innerHTML = `If you like ${userTaste.input}, you'll also like...`
+    resultMessage.append(msg);
+    const resultsSection = document.getElementById("resultsListMountNode");
+    resultsSection.innerHTML = '';
+    const list = document.createElement("ul");
+    console.log('results: ', taste.Similar.Results);
+    console.log('length', taste.Similar.Results.length);
+    for (const {Name, Type, wTeaser, wUrl, yUrl, yID} of taste.Similar.Results){
+        if(taste.Similar.Results.length!==0){
+            //render list
+            resultsSection.appendChild(list);
+            list.classList.add("row", "g-2");
+            const li = document.createElement("li");
+            li.classList.add("col", "col-lg-4", "col-md-6", "col-sm-12")
+            li.innerHTML = `<div class="card">
+            <iframe src=${yUrl} alt='No video available' class="card-img-top"></iframe>
+            <div class="card-body">
+            <h2>${Name} <small class="text-muted">(${Type})</small></h2>
+            <p>${wTeaser}</p>
+            <a href=${wUrl} target="_blank" class="btn btn-primary">Learn more</a>
+            </div>
+            </div>`
+            list.append(li);
+        } else if(taste.Similar.Results.length===0){
+        console.log('no results');
+        const noResults = document.createElement("p");
+        noResults.innerHTML = `Sorry we can't find any tastes to match yours. Try something else.`
+        resultsSection.appendChild(noResults);
+        }
+    }
+};
+
+async function getTastes(data, handler=renderList) {
+    console.log('getTastes input: ', data);
+    const joinedData = data.join('+');
+    const queryString = `?q=${joinedData}`
+    console.log('query string: ', queryString)
+    try {
+        const response = await fetch(`http://localhost:3000/api/v1/similar_tastes${queryString}`)
+        if (response.ok) {
+            const result = await response.json();
+            console.log('result', result);
+            renderList(result);
+            } else {
+            console.log('status',response.status, response.statusText);
+            throw new Error(response);
+        } } catch (err) {
+            console.log(err);
+        }};
+
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(searchForm);
+    localStorage.setItem('userTaste', JSON.stringify(Object.fromEntries(formData)));
+    const data = Object.fromEntries(formData);
+    const dataArray = data.input.split(' ')
+    console.log('data array: ', dataArray);
+    getTastes(dataArray);
+    searchForm.reset();
+});
+
+
+
---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const fetch = require("node-fetch");
+
+const express = require('express');
+cors = require('cors');
+
+const {
+    PORT,
+    API_KEY,
+    PROXY_URL,
+    API_URL
+} = process.env
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/api/v1/similar_tastes/', (req, res) => {
+    const taste = req.query.q;
+    console.log('req: ', taste);
+
+    (async()=>{
+        const PROXY_URLParameters = new URLSearchParams();
+        const API_URLParameters = new URLSearchParams();
+        API_URLParameters.set("info", 1);
+        API_URLParameters.set("limit", 6);
+        API_URLParameters.set("k", API_KEY);
+        API_URLParameters.set("q", taste );
+
+        const FULL_API_URL = `${API_URL}${API_URLParameters.toString()}`;
+        console.log('FULL_API_URL', FULL_API_URL);
+        
+        PROXY_URLParameters.set('url', FULL_API_URL);
+        const fullURL = `${PROXY_URL}${PROXY_URLParameters.toString()}`;
+        console.log('fullURL:', fullURL);
+        try {
+            const response = await fetch(fullURL);
+            if (response.ok) {
+                const data = await response.json();
+                data.contents = JSON.parse(data.contents); // For some reason we have to do this twice 
+                console.log('data', data.contents);
+                res.status(200).json(data.contents);
+                } else {
+                res.status(404).send(data.contents);
+                }
+            } catch (err) {
+                console.log(err.message);
+            }
+    })();
+});
+
+app.listen( PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
